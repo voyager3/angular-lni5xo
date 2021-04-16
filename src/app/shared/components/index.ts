c30128d07ea384c86fb3d1b686f2c97707e0c092
SvetlozarAnev <aneef.svtlzr@gmail.com>
2021-04-16 17:31:15 +0300

    self-assessment component indexed

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,11 +2,12 @@ import { FormErrorComponent } from './form-error/form-error.component';
 import { CardButtonComponent } from './card-button/card-button.component';
 import { CardButtonFilterComponent } from './card-button-filter/card-button-filter.component';
 import { KendoCustomSearchComponent } from './kendo-custom-search/kendo-custom-search.component';
-import { HierarchySelectorComponent } from './hierarchy-selector/hierarchy-selector.component'
-import { OrderableListComponent } from './orderable-list/orderable-list.component'
-import { SlideAccordionComponent } from './slide-accordion/slide-accordion.component'
-import { MetaInfoComponent } from './meta-info/meta-info.component'
-import { TrainSelectorComponent } from './train-selector/train-selector.component'
+import { HierarchySelectorComponent } from './hierarchy-selector/hierarchy-selector.component';
+import { OrderableListComponent } from './orderable-list/orderable-list.component';
+import { SlideAccordionComponent } from './slide-accordion/slide-accordion.component';
+import { MetaInfoComponent } from './meta-info/meta-info.component';
+import { TrainSelectorComponent } from './train-selector/train-selector.component';
+import { SelfAssessmentLevelsComponent } from './self-assessment/self-assessment.component';
 
 export const components = [
     FormErrorComponent,
@@ -17,5 +18,6 @@ export const components = [
     OrderableListComponent,
     SlideAccordionComponent,
     MetaInfoComponent,
-    TrainSelectorComponent
+    TrainSelectorComponent,
+    SelfAssessmentLevelsComponent
 ]
